f878a64d88d119486cdbc6ed7fde14175206d28b
Trevor <trevorj342@gmail.com>
2021-08-01 01:45:40 -0400

    i finally disabled idling, only took like 3 days of fiddling

--- DawnVRMod/VRModules/HarmonyPatches.cs
+++ DawnVRMod/VRModules/HarmonyPatches.cs
@@ -13,15 +13,36 @@ namespace DawnVR
         public static void Init(HarmonyLib.Harmony hInstance)
         {
             HarmonyInstance = hInstance;
-            PatchPre(typeof(_1EB728BCC.T_A7E3390E).GetMethod("LateUpdate"), "EyeHeadAnimator_LateUpdate");
+            // Input Handling
             PatchPre(typeof(T_6FCAE66C).GetMethod("GetAxisVector3"), "VRVector3Axis");
+            // Disable Idling
+            PatchPre(typeof(T_7C97EEE2).GetMethod("GetIdleExtraName"), "GetIdleExtraName");
+            PatchPost(typeof(T_C3DD66D9).GetMethod("Start"), "PostCharControllerStart");
         }
 
         private static void PatchPre(MethodInfo original, string prefixMethodName) => HarmonyInstance.Patch(original, new HarmonyLib.HarmonyMethod(typeof(HarmonyPatches).GetMethod(prefixMethodName)));
         private static void PatchPost(MethodInfo original, string postfixMethodName) => HarmonyInstance.Patch(original, null, new HarmonyLib.HarmonyMethod(typeof(HarmonyPatches).GetMethod(postfixMethodName)));
 
-        //todo: not working, animation handled by CharController?
-        public static bool EyeHeadAnimator_LateUpdate() => false;
+        public static bool GetIdleExtraName(ref string __result)
+        {
+            // todo: this causes an error to be spat out every frame, might want to replace at some point
+            // CharAnimSet.GetIdleExtraName
+            __result = "";
+            return false;
+        }
+
+        public static void PostCharControllerStart(T_C3DD66D9 __instance)
+        {
+            foreach (AnimationState state in __instance.m_animStates)
+            {
+                if (state.name.ToLower().Contains("idle"))
+                {
+                    state.weight = 0;
+                    state.speed = 0;
+                    state.time = 0;
+                }
+            }
+        }
 
         public static bool VRVector3Axis(ref Vector3 __result, eGameInput _1A16DF67C, eGameInput _19E4D962D, eGameInput _19F48D18E)
         {
@@ -37,55 +58,14 @@ namespace DawnVR
             HarmonyInstance = hInstance;
             PatchPre(typeof(T_A6E913D1).GetMethod("IsAllowDebugOptions"), "ReturnTrue");
             PatchPre(typeof(T_A6E913D1).GetMethod("IsTool"), "ReturnTrue");
-            PatchPre(typeof(T_C3DD66D9).GetMethod("SetupIdle"), "DontRunMe");
-            PatchPre(typeof(T_C3DD66D9).GetMethod("UpdateBlendIdle"), "UpdateIdleAnim");
+
+            
         }
 
         public static bool ReturnTrue(ref bool __result)
         {
             __result = true;
             return false;
-        }
-
-        public static bool UpdateIdleAnim(T_C3DD66D9 __instance)
-        {
-            // todo: funfact, this leads to terrifying results, need to figure this out
-            typeof(T_C3DD66D9).GetField("_19C474D6C", HarmonyLib.AccessTools.all).SetValue(__instance, null);
-            if (__instance.IsWalking)
-            {
-                __instance.m_currProgression = (__instance.m_targetProgression = 0f);
-                if (__instance.m_targetProgression == 0f)
-                {
-                    float num = Mathf.Ceil(__instance.m_currProgression) * __instance.CurrentAnimSet.animSet.c_maxIdleFade * Time.deltaTime;
-                    __instance.m_currProgression = Mathf.Clamp(__instance.m_currProgression - num, 0f, 3f);
-                }
-                else
-                {
-                    float num2 = Mathf.Ceil(__instance.m_currProgression) * __instance.CurrentAnimSet.animSet.c_maxFade * Time.deltaTime;
-                    __instance.m_currProgression = Mathf.Clamp(__instance.m_currProgression - num2, 0f, 3f);
-                }
-                __instance.m_currentFade = __instance.m_currProgression - Mathf.Floor(__instance.m_currProgression);
-                __instance.m_currFadeSpeed = __instance.CurrentAnimSet.animSet.c_maxIdleFade;
-                __instance.m_charAnimState = T_7C97EEE2.eCharMoveAnim.kWalk;
-                __instance.UpdateWalkAnims();
-            }
-            else
-            {
-                /*for (int i = 1; i < __instance.m_animStates.Length; i++)
-                {
-                    __instance.ResetAnimState(i, false);
-                    typeof(T_C3DD66D9)
-                }
-                if (__instance.IsAnimValid(T_7C97EEE2.eCharMoveAnim.kIdle))
-                {
-                    __instance.m_animStates[0].weight = 1f;
-                    if (__instance.CurrentAnimSet.HasProp())
-                    {
-                        __instance.m_propAnimStates[0].weight = 1f;
-                    }
-                }*/
-            }
-            return true;
         }
 
         public static bool DontRunMe()
