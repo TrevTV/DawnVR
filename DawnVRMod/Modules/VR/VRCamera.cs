00e3c912e1985c714cec555198b8880bdbf6c9af
Trevor <trevorj342@gmail.com>
2021-08-28 23:39:16 -0400

    fix ui issues with non-1920x1080 displays

--- DawnVRMod/Modules/VR/VRCamera.cs
+++ DawnVRMod/Modules/VR/VRCamera.cs
@@ -21,7 +21,7 @@ namespace DawnVR.Modules.VR
             uiRenderer = transform.Find("UIRenderer");
             uiRenderer.localScale = new Vector3(0.25f, 0.15f, 0.15f);
             uiCamera = GameObject.Find("/UIRoot/Camera").GetComponent<Camera>();
-            RenderToVRTexture = new RenderTexture(1920, 1080, 1);
+            RenderToVRTexture = new RenderTexture(Screen.width, Screen.height, 1);
             uiCamera.targetTexture = RenderToVRTexture;
             uiCamera.clearFlags = CameraClearFlags.Color;
             uiCamera.backgroundColor = new Color(0, 0, 0, 0);
