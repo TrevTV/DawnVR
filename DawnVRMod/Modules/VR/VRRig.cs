8c50e912abc0d844b0915747bcdb1aa5e7245b29
Trevor <trevorj342@gmail.com>
2021-12-21 21:17:03 -0500

    move steamvr lib to a subfolder

--- DawnVRMod/Modules/VR/VRRig.cs
+++ DawnVRMod/Modules/VR/VRRig.cs
@@ -221,8 +221,9 @@ namespace DawnVR.Modules.VR
 			if (ChloeComponent != null && transform.parent == ChloeComponent.transform)
 			{
                 Vector3 offset = Camera.transform.localPosition - lastPos;
+				offset = Camera.Holder.TransformDirection(offset);
 				offset.y = 0;
-                ChloeComponent.transform.localPosition += offset;
+				ChloeComponent.transform.localPosition += offset;
 				Camera.Holder.localPosition -= offset;
                 lastPos = Camera.transform.localPosition;
             }
@@ -294,10 +295,25 @@ namespace DawnVR.Modules.VR
             }
         }
 
+		public float floorOffset;
+		public float irlHeight;
+
 		public void BeginCalibration()
         {
 			// todo: calibration
+/*if (sitStandMode.Equals(PlayerSitStandMode.Seated))
+        {
+            seatedOffset = irlHeight - player.cam.localPosition.y - floorOffset;
+            player.heightOffset = seatedOffset;
+            scale = ((player.height) / (irlHeight / 10f));
         }
+			else
+			{
+				floorOffset = irlHeight - player.cam.localPosition.y;
+				player.heightOffset = -floorOffset;
+				scale = ((player.height) / (irlHeight / 10f));
+			}*/
+		}
 
 		public void UpdateCachedChloe(T_C3DD66D9 newChloe, bool updateParent = true)
         {
